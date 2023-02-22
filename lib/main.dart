d1624ace0d25f43291065f650e55c38fc119415d
Agzhan Batyrbek <agjanbatyrbek855@gmail.com>
2023-02-23 01:16:56 +0800

    Created the UserHome and Product Detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce/app/pages/providers.dart';
 import 'package:ecommerce/app/pages/sign_in_page.dart';
+import 'package:ecommerce/app/pages/user/user_home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -31,22 +32,7 @@ class MyApp extends ConsumerWidget {
       ),
       home: AuthWidget(
           adminSignedInBuilder: (context) => const AdminHome(),
-          signedInBuilder: (context) => Scaffold(
-                body: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Text("Signed In in"),
-                      ElevatedButton(
-                        onPressed: () {
-                          ref.read(firebaseAuthProvider).signOut();
-                        },
-                        child: const Text("Sign out"),
-                      )
-                    ],
-                  ),
-                ),
-              ),
+          signedInBuilder: (context) => const UserHome(),
           nonSignedInBuilder: (_) => const SignInPage()),
     );
   }
