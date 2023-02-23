ac579d790cce926275d941155253c2a544299259
Agzhan Batyrbek <agjanbatyrbek855@gmail.com>
2023-02-24 00:48:14 +0800

    Implemented saving the User in the Database

--- lib/services/firestore_services.dart
+++ lib/services/firestore_services.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce/models/product_model.dart';
+import 'package:ecommerce/models/user_data.dart';
 
 class FirestoreService {
   FirestoreService({required this.uid});
@@ -27,5 +28,16 @@ class FirestoreService {
   ) async {
     final docId = firestore.collection("products").doc().id;
     await firestore.collection("products").doc(docId).set(product.toMap(docId));
+  }
+
+  Future<void> addUser(
+    UserData user,
+  ) async {
+    await firestore.collection("users").doc(user.uid).set(user.toMap());
+  }
+
+  Future<UserData?> getUser(String uid) async {
+    final doc = await firestore.collection("users").doc(uid).get();
+    return doc.exists ? UserData.fromMap(doc.data()!) : null;
   }
 }
